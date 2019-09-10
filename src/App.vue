66197bfb461ad37d1935b4f4f7d476db4890a0a4
Hyunsoo Han <hyunsu_han@fursys.com>
2019-09-10 13:17:08 +0900

    msg changes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to 현수 App"/>
+    <HelloWorld msg="Welcome to 현수 App!!"/>
   </div>
 </template>
 
